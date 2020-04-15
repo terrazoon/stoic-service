676cb09445449f18959913d7f0cff01401afcf49
Kenneth Kehl <k3nn3th.k3hl@gmail.com>
2020-04-15 16:46:36 -0700

    add dynamodb for email addresses, hookup subscribe and unsubscribe

--- src/controllers/quote_controller.py
+++ src/controllers/quote_controller.py
@@ -1,27 +1,13 @@
 import json
 from random import randrange
 
-# import requests
-
-
-# def chuck_norris_joke(event, context):
-#
-#     joke_response = requests.get("http://api.icndb.com/jokes/random")
-#     joke_as_dict = json.loads(joke_response.text)
-#     joke = joke_as_dict["value"]["joke"]
-#     print(f"Here's a joke: {joke}")
-#     response = {
-#         "statusCode": 200,
-#         "body": json.dumps(joke)
-#     }
-#     return response
 from src.services.email_service import EmailService
 
 
 def _get_image(image_data):
-
     image = f"<div><image src='{image_data}' alt='image'</div>"
     return image
+
 
 def stoic_quote(event, context):
     with open('stoic_quotes.json') as f:
@@ -30,16 +16,7 @@ def stoic_quote(event, context):
     num_quotes = len(quote_list)
     myrand = randrange(num_quotes)
     quote = quote_list[myrand]
-    print(f"Author: {quote['Author']} Quote: {quote['Quote']}")
 
-    # with open('images.json') as f:
-    #     images = json.load(f)
-    #
-    # myhtml = "<html><body><h2>" + quote['Quote'] + "/h2" + "<br/><br/>" + quote['Author']
-    # # myhtml += _get_image(images[quote['Author']])
-    # myhtml += "</body></html>"
-    #
-    # print(myhtml)
     print(f"about to send email from controller with {quote}")
     EmailService.send_email(quote)
 
---
+++ src/controllers/subscription_controller.py
@@ -0,0 +1,41 @@
+import json
+
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('emailAddresses')
+
+
+def subscribe(event, context):
+    email = event['pathParameters']['email']
+
+    response = table.get_item(
+        Key={
+            'email': email
+        }
+    )
+
+    if response.get("Item") is None:
+        table.put_item(TableName='emailAddresses', Item={'email': email})
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(email)
+    }
+    return response
+
+
+def unsubscribe(event, context):
+    email = event['pathParameters']['email']
+
+    response = table.delete_item(
+        Key={
+            'email': email
+        }
+    )
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(email)
+    }
+    return response
